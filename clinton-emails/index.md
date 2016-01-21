48688e417d1cd82c080bae0c8475aed4ec1dc759
Sergiusz <sergiusz.bleja@timgroup.com>
2016-01-21 10:45:26 +0000

    Adding slides

---
+++ clinton-emails/index.md
@@ -0,0 +1,37 @@
+---
+title       : Kaggle dojo
+subtitle    : 
+author      : 
+job         : 
+framework   : io2012        # {io2012, html5slides, shower, dzslides, ...}
+highlighter : highlight.js  # {highlight.js, prettify, highlight}
+hitheme     : tomorrow      # 
+widgets     : []            # {mathjax, quiz, bootstrap}
+mode        : selfcontained # {standalone, draft}
+knit        : slidify::knit2slides
+---
+
+## Kaggle
+
+* Kaggle hosts data driven insights competitions online
+* Popular in the machine learning community
+* Recently they started hosting interesting data sets such as...
+
+
+--- 
+
+## Hillary Clinton's emails!
+
+* Background: Hillary Clinton was using personal email during her time as Secretary of state. 
+* Caused controversy due it possibly violating transperancy protocols.
+* 7000+ pdfs parsed and cleaned by kaggle
+
+
+
+---
+
+![plot of chunk from](assets/fig/from-1.png)
+
+
+
+
--- shitClintonSays.R
+++ shitClintonSays.R
@@ -13,23 +13,24 @@ main <- function() {
   freq.to.h = wordFrequencyFor(emails.to.hill)
   freq.from.h = wordFrequencyFor(emails.from.hill)
 
-  in.from.but.not.in.to <- merge(freq.from.h, freq.to.h, by = "word", all.x = TRUE, all.y = TRUE)
-
-  #wordcloud(em, min.freq = 50, rot.per=0.35,  colors=brewer.pal(8, "Dark2"))
-  #  wordcloud(freq.to.h$word, freq.to.h$freq, min.freq = 20, random.order = FALSE, colors=brewer.pal(8, "Dark2"))
-  #  wordcloud(freq.from.h$word, freq.from.h$freq, min.freq = 20, random.order = FALSE, colors=brewer.pal(8, "Dark2"))
-
   printWordCloudFor(freq.from.h)
   printWordCloudFor(freq.to.h, min.freq = 100)
 
-  all.merge = in.from.but.not.in.to 
-  all.merge[, from.h := freq.x/sum(freq.x, na.rm = TRUE)]
-  # all.merge[, from.h := rank(-freq.x)]
-  all.merge[, to.h := freq.y/sum(freq.y, na.rm = TRUE)]
-  # all.merge[, to.h := rank(freq.y)]
 
-  head(all.merge[order(from.h, decreasing = TRUE)], n = 10)
-  head(all.merge[order(to.h, decreasing = TRUE)], n = 10)
+
+
+  #   in.from.but.not.in.to <- merge(freq.from.h, freq.to.h, by = "word", all.x = TRUE, all.y = TRUE)
+  #   all.merge = in.from.but.not.in.to 
+  #   setnames(all.merge, c("freq.x", "freq.y"), c("from.clinton", "to.clinton"))
+  # 
+  #   comparison.cloud(all.merge, max.words = 40, random.order = FALSE)
+#   all.merge[, from.h := freq.x/sum(freq.x, na.rm = TRUE)]
+  #   all.merge[, from.h := rank(-freq.x)]
+  #   all.merge[, to.h := freq.y/sum(freq.y, na.rm = TRUE)]
+  #   all.merge[, to.h := rank(freq.y)]
+  # 
+  #   head(all.merge[order(from.h, decreasing = TRUE)], n = 10)
+  #   head(all.merge[order(to.h, decreasing = TRUE)], n = 10)
   # in.from.but.not.in.to[order(freq.x, decreasing = TRUE)]
   # in.from.but.not.in.to[order(freq.y, decreasing = TRUE)]
 }
@@ -47,7 +48,7 @@ wordFrequencyFor <- function(text) {
   em <- tm_map(em, removeNumbers)
   em <- tm_map(em, stemDocument)
   # em <- tm_map(em, stemCompletion, dictionary = orig)
-  my.stop <- c(stopwords("english"), "also", "will")
+  my.stop <- c(stopwords("english"), "also", "will", "hrodclintonemailcom")
   em <- tm_map(em, removeWords, my.stop)
   
   dtm <- TermDocumentMatrix(em)
